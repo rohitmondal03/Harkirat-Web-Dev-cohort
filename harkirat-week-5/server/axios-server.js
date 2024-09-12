4b896b3d0baff7e95f37cdb072e30c025bd742dd
Rohit <rohitmondall8000@gmail.com>
2024-09-12 22:29:57 +0530

    week 5: completed axios

--- harkirat-week-5/server/axios-server.js
+++ harkirat-week-5/server/axios-server.js
@@ -1,12 +1,25 @@
-const express = require("express");
+import express from "express"
+import bodyParser from "body-parser";
+import cors from "cors"
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+
 app.post("/post", (req, res) => {
   const data = req.body;
   res.status(200).json(data);
+})
+
+app.get("/post", (req, res) => {
+  res.json({
+    message: "Got a GET request !!"
+  })
 })
 
 
